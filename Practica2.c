6f35628f88744f838ec312dc1fecf2ba3eefbea6
ie702272 <ie702272@iteso.mx>
2018-02-21 07:10:15 -0600

    actualizacion

--- Practica2.c
+++ Practica2.c
@@ -44,6 +44,7 @@
 #include "FIFO.h"
 #include "PIT.h"
 #include "DAC.h"
+#include "fsl_dac.h"
 
 //the system clock of the Kinetis
 #define SYSTEM_CLOCK CLOCK_GetBusClkFreq()
@@ -52,19 +53,49 @@
 //pit number to count
 #define LDVAL_trigger  (SYSTEM_CLOCK * PERIOD)/20
 
+int8_t h1[7] = {
+0.07840464525404556,
+0.17707825519483075,
+0.22014353249171387,
+0.2759015644497544,
+0.22014353249171387,
+0.17707825519483075,
+0.07840464525404556
+};
+
+
+int8_t h2[7] ={
+-0.08857280384687653,
+-0.20001387909943527,
+-0.13289448474069163,
+0.7755518089951376,
+-0.13289448474069163,
+-0.20001387909943527,
+-0.08857280384687653
+};
+
+uint8_t contador=0;
+int8_t conv[7] = {0};
+
 adc16_config_t adc16ConfigStruct;
 adc16_channel_config_t adc16ChannelConfigStruct;
 ADC_Type *base = ADC0;
 uint32_t channelGroup = 0;
+dac_config_t dacConfigStruct;
 
 void PIT0_IRQHandler ()
 {
     PIT_ClearStatusFlags (PIT, kPIT_Chnl_0, kPIT_TimerFlag);
 
     sint8 read = (sint8) ADC_Values();
+    for(contador;contador<7;contador++){
+    	conv[contador] = read*h1[contador];
+
+        DACFunction(PIT_0, (uint8) conv[contador]);
+    }
+    contador=0;
     //printf(" %d\n",read);
     //push (read);
-    DACFunction(PIT_0, (uint8) read*10);
 
     PIT_SetTimerPeriod (PIT, kPIT_Chnl_0, LDVAL_trigger);
     PIT_StartTimer (PIT, kPIT_Chnl_0);
@@ -95,6 +126,12 @@ int main (void)
 
     PIT_SetTimerPeriod (PIT, kPIT_Chnl_0, LDVAL_trigger);
     PIT_StartTimer (PIT, kPIT_Chnl_0);
+
+//    Configures the DAC.
+//    DAC_GetDefaultConfig(&dacConfigStruct);
+//    DAC_Init(DAC0, &dacConfigStruct);
+//    DAC_Enable(DAC0, true);
+//    DAC_SetBufferReadPointer(DAC0, 0U);
 
     while (1)
     {
